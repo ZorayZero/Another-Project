f566150909ccf192634a329f810d81311e536444
Zoray <Gerrick.Keshawn.Jones@gmail.com>
2023-10-26 18:39:20 -0400

    added more variables :(

--- Another Project/Assets/Scripts/playerMovement.cs
+++ Another Project/Assets/Scripts/playerMovement.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    [Header("references")]
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce = 3;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
+    void moveInput()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+    }
+
+ 
+       
 }
