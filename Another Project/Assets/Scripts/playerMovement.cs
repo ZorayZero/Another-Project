cc76ebf0023cbe9099966e8ee77fd3b8f2a64d70
Zoray <Gerrick.Keshawn.Jones@gmail.com>
2023-10-30 18:17:58 -0400

    The player can now move and jump.

--- Another Project/Assets/Scripts/playerMovement.cs
+++ Another Project/Assets/Scripts/playerMovement.cs
@@ -7,8 +7,8 @@ public class playerMovement : MonoBehaviour
 {
     [Header("references")]
     [SerializeField] float speed = 5f;
-    [SerializeField] float jumpForce = 3;
-    Rigidbody rb;
+    [SerializeField] float jumpForce = 300;
+    [SerializeField] Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +19,20 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var vel = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed;
+
+        vel.y = rb.velocity.y;
+        rb.velocity = vel;
        
+        if (Input.GetKeyDown(KeyCode.Space))
+        { rb.AddForce(Vector3.up * jumpForce);  }
+
     }
 
-    void moveInput()
-    {
-        float xMove = Input.GetAxis("Horizontal");
-        float zMove = Input.GetAxis("Vertical");
-    }
+  
+      
+         
+    
 
  
        
